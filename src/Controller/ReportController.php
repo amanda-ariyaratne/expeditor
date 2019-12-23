409444a9df4f90a5a7360fdb11eff56c27b47519
Amanda Ariyaratne <aariyaratne@gmail.com>
2019-12-23 15:02:44 +0530

    basic form

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\QuarterlySalesReportType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/report")
+ */
+class ReportController extends AbstractController
+{
+    /**
+     * @Route("/quarterly/sales", name="report_quarterly_sales")
+     */
+    public function getQuarterlySalesReport(Request $request): Response
+    {
+        $defaultData = [];
+        $form = $this->createForm(QuarterlySalesReportType::class, $defaultData);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+        }
+        return $this->render('report/quarterly_sales_report.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    
+}
