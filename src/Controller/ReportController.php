91ad801e92c9dbe283bc18499fe834a3561ba6b1
“wageesha_e_b” <wageesha26@gmail.com>
2019-12-24 22:19:00 +0530

    commit report files

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\QuarterlySalesReportType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Store;
+
+/**
+ * @Route("/report")
+ */
+class ReportController extends AbstractController
+{
+    /**
+     * @Route("/quarterly/sales", name="report_quarterly_sales", methods={"GET"})
+     */
+    public function getQuarterlySalesReport(Request $request): Response
+    {
+        $defaultData = [];
+        $form = $this->createForm(QuarterlySalesReportType::class, $defaultData, [
+            'entityManager' => $this->getDoctrine()->getManager(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+        }
+        return $this->render('report/quarterly_sales_report.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
